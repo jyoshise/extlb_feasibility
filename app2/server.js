3230e544723a8da98d3efd3ca4959eb817e38708
jyoshise <yoshise@gmail.com>
2020-08-12 11:16:41 +0900

    add app2

---
+++ app2/server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var handleRequest = function(request, response) {
+  console.log('Received request for URL: ' + request.url);
+  response.writeHead(200);
+  response.end('This is App 2');
+};
+var www = http.createServer(handleRequest);
+www.listen(8080);
